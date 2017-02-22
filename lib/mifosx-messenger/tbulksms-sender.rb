9bf52672356e45cdc92a8d4c7dfac3dbe83af031
Doctor Fox <drfox29@yahoo.co.uk>
2017-02-22 17:29:53 +0100

    Update tbulksms-sender.rb

--- lib/mifosx-messenger/tbulksms-sender.rb
+++ lib/mifosx-messenger/tbulksms-sender.rb
@@ -2,23 +2,23 @@ require 'httpclient'
 require_relative 'message-sender'
 
 module MifosXMessenger
-	class EBulkSMSSender < MessageSender
+	class TBulkSMSSender < MessageSender
 		@uri = nil
 		@params = nil
 
 		def initialize(options = {})
-			@uri = options['uri'] || 'http://api.ebulksms.com:8080/sendsms'
+			@uri = options['uri'] || 'http://login.tbulksms.com/API/WebSMS/Http/v1.0a/index.php'
 			@params = options
-			@params['flash'] ||= 0
-			@params['sender'] ||= 'Mifos SMS'
+			@params['format'] ||= 'json'
+			@params['sender'] ||= 'WEBSMS'
 		end
 
 		def send_sms(number, message)
 			params = @params
-			params['recipients'] = number
-			params['messagetext'] = message
+			params['to'] = number
+			params['message'] = message
 
-			client = HTTPClient.new(:agent_name => 'MifosWebAppRuby/0.1')
+			client = HTTPClient.new(:agent_name => 'MyAgent/0.1')
 			client.post(@uri, params)
 		end
 	end
